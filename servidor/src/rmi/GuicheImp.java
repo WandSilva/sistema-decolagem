ab868e26d8dcb8666d7590461b7e453961e7dbe7
Wanderson <bsilva.wanderson@gmail.com>
2017-08-19 19:51:38 -0300

    iniciando busca servidor-servidor

--- servidor/src/rmi/GuicheImp.java
+++ servidor/src/rmi/GuicheImp.java
@@ -11,9 +11,7 @@ import java.util.ArrayList;
  * Created by wanderson on 28/07/17.
  */
 public class GuicheImp extends UnicastRemoteObject implements Guiche {
-    
-    private String nomeServidor;
-    
+
     @Override
     public void comprarRota(String rota) throws RemoteException {
         this.controller.comprar(rota);
@@ -23,21 +21,14 @@ public class GuicheImp extends UnicastRemoteObject implements Guiche {
 
     protected GuicheImp(String nomeServidor) throws RemoteException, FileNotFoundException {
         super();
-        this.nomeServidor = nomeServidor;
-        this.controller = new Controller();
+        this.controller = new Controller(nomeServidor);
         this.controller.criarRotas();
     }
 
     @Override
     public ArrayList<String> pesquisarRotas(String origem, String destino) throws RemoteException {
-
         
-        
-        ArrayList<String> teste = controller.buscarRotas(origem, destino);
-        for (int i = 0; i < teste.size(); i++) {
-            teste.set(i, teste.get(i) + " -> " + this.nomeServidor);
-        }
-        return teste;
+        return controller.buscarRotas(origem, destino);
     }
 
     @Override
@@ -48,11 +39,6 @@ public class GuicheImp extends UnicastRemoteObject implements Guiche {
     @Override
     public void carregarServidores() throws RemoteException {
         controller.carregarServidores();
-    }
-
-    @Override
-    public String getNomeServidor() {
-        return nomeServidor;
     }
 
 }
